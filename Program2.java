b6ac621dd6241856571ca3566cfb560988e892ea
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-26 19:03:06 +0530

    Second pgm

---
+++ Program2.java
@@ -0,0 +1,11 @@
+class XYZ{
+	public static void run(){
+		System.out.println("Bhaag");
+	}
+	public static void main(String[] args){
+		System.out.println("Start main");
+		run();
+		System.out.println("End main");
+	}
+
+}
