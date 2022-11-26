e9b8927cf9a6d4fa95e9e3acef0d0ca279216ba7
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-26 22:46:50 +0530

    char is vowel or consonant codeprogram20.java

---
+++ program20.java
@@ -0,0 +1,11 @@
+class assi1{
+	public static void main(String[]args){
+		char ch='A';
+
+		if(ch=='A'|| ch=='a' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch=='u' || ch=='U'){			System.out.println(ch+"is vowel");
+                	}
+	                  else{
+		                System.out.println(ch+" is consonant");
+	}
+}
+}
