e64ffd69deff8340b7e01ca632a49881990f2765
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-26 22:39:32 +0530

    potive or negativecodeprogram19.java

---
+++ program19.java
@@ -0,0 +1,11 @@
+class assi{
+	public static void main(String[]rgs){
+		int var=2;
+		if(var>0){
+			System.out.println(var+"positive number");
+		}
+		else{
+			System.out.println(var+"negative number");
+		}
+	}
+}
