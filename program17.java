2e81abada4de31fd1ebd2523489beb111841d1c5
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-26 22:29:06 +0530

    switch codeprogram17.java

---
+++ program17.java
@@ -0,0 +1,21 @@
+class Switchex{
+	public static void main(String[]args){
+		char ch='A';
+
+		switch(ch){
+			case 'A':
+				System.out.println('A');
+				break;
+			case 'B':
+				System.out.println('B');
+				break;
+			case 67:
+				System.out.println(67);
+				break;
+			default:
+				System.out.println("default");
+				break;
+
+		}
+	}
+}
