392fe39610e5b7c845a8cde7d66b64d6a681dcc5
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-27 12:01:46 +0530

    max numberprogram24.java

---
+++ program24.java
@@ -0,0 +1,15 @@
+class max{
+	public static void main(String[]args){
+		int a=1; int b=2; int c=3;
+		if(a>b){
+			System.out.println(a+"is greater");
+		}
+		else if(b>c){
+			System.out.println(b+"is greater");
+		}
+		else if(c>a){
+			System.out.println(c+"is greater");
+		}
+
+	}
+}
