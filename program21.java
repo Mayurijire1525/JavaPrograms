41c292870aba43074cf31202ef3fe499d6fe2ebc
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-27 11:46:04 +0530

    Acc. to month print no of daysprogram21.java

---
+++ program21.java
@@ -0,0 +1,46 @@
+class Switchs{
+	public static void main(String[]args){
+		int var=7;
+		switch(var){
+			case 1:
+				System.out.println("January has 31 days");
+				break;
+			case 2:
+				System.out.println("Fenruary has 28/29 days");
+				break;
+			case 3:
+				System.out.println("March has 31 days");
+				break;
+			case 4:
+				System.out.println("April has 30 days");
+				break;
+			case 5:
+				System.out.println("May has 31 days");
+				break;
+			case 6:
+				System.out.println("June has 30 days");
+				break;
+			case 7:
+				System.out.println("July has 31 days");
+				break;
+			case 8:
+				System.out.println("August has 30 days");
+				break;
+			case 9:
+				System.out.println("September has 31 days");
+				break;
+			case 10:
+				System.out.println("October has 30 days");
+				break;
+			case 11:
+				System.out.println("November has 31 days");
+				break;
+			case 12:
+				System.out.println("December has 30 days");
+				break;
+			default:
+				System.out.println("Out of range");
+				break;
+		}
+	}
+}
