f0a1392e7b5575eb05b3c2fde8a49b75df85b66e
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-28 14:57:25 +0530

    simple array ex

---
+++ program60.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Array{
+	public static void main(String[]args){
+		char carr[]=new char[5];
+		
+		carr[0]='A';
+		carr[1]='B';
+		carr[2]='C';
+		carr[3]='D';
+		carr[4]='E';
+
+	
+		for(int i=0; i<5; i++){
+			System.out.println(carr[i]);
+			
+		}
+		
+	}
+}
