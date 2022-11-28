de530dfb21b57d80cdb9a89a88a40c860bf95d06
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-28 13:39:03 +0530

    reverse (triangle) pattern

---
+++ program46.java
@@ -0,0 +1,11 @@
+class pattern{
+	public static void main(String[]args){
+		for(int row=1; row<=4; row++){
+			for(int col=4; col>=row; col--){
+				System.out.print("* ");
+				
+			}
+			System.out.println();
+		}
+	}
+}
