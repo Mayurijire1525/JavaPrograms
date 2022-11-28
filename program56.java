290ae9dfbb14426519d161f613aa12fc8a1d8e23
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-28 14:28:22 +0530

    player info input from user ex

---
+++ program56.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class player{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter player name");
+		String name=sc.next();
+
+		System.out.println("Enter jersy num");
+		int jer=sc.nextInt();
+
+		System.out.println("player name="+name);
+		System.out.println("Jersy num="+jer);
+	}
+}
