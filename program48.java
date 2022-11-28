55dfb0fa15ad8fcc6c2b6401340734d9b2cf6c1a
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-28 13:43:47 +0530

    square always starts with 1 (reverse triangle) pattern

---
+++ program48.java
@@ -0,0 +1,13 @@
+class pattern{
+	public static void main(String[]args){
+		for(int row=1; row<=4; row++){
+			int num=1;
+			for(int col=4; col>=row; col--){
+				System.out.print(num*num+" ");
+				num++;
+				
+			}
+			System.out.println();
+		}
+	}
+}
