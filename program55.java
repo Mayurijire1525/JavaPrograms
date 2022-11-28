befe6433576fa251c63ea690ebc8d787088a013c
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-28 14:23:35 +0530

    Package ex

---
+++ program55.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Package{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter input");
+
+		int var1=sc.nextInt();
+		int var2=sc.nextInt();
+
+		System.out.println(var1+var2);
+	}
+}
