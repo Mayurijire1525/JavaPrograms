88ad14069724e2239348df9aaf6955d70e664af8
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-28 13:56:34 +0530

    space(reverse triangle) pattern

---
+++ program49.java
@@ -0,0 +1,14 @@
+class pattern{
+	public static void main(String[]args){
+		for(int row=1; row<=4; row++){
+			for(int sp=3; sp>=row; sp--){
+				System.out.print(" ");
+			}
+			for(int col=1; col<=row; col++){
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+	}
+}
