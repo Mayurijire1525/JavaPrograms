1001fbaeddddc681b5a1f53ec6cc4ea2d0f1aac2
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-29 11:25:55 +0530

    ways constructor

---
+++ program75.java
@@ -0,0 +1,13 @@
+class constructor{
+	public static void main(String[]args){
+		String str1=new String();
+		String str2=new String("Java");
+		char []arr={'J','a','v','a'};
+
+		String str3=new String(arr);
+
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+}
