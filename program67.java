31094e74089c272ba1c42fab8b2098fdd007e30a
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-29 10:31:18 +0530

    String demo

---
+++ program67.java
@@ -0,0 +1,11 @@
+class stringde{
+	public static void main(String[]args){
+		String str1="core2web";
+		String str2="core2web";
+
+		System.out.println(str1);
+		System.out.println(str2);
+
+		System.out.println(str1=str2);
+	}
+}
