0c371caf490683d4d515782afd99705a59915260
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-29 10:37:45 +0530

    String demo

---
+++ program68.java
@@ -0,0 +1,13 @@
+
+class stringde{
+	public static void main(String[]args){
+		String str1=new String("core2web");
+		String str2=new String("core2web");
+
+		System.out.println(str1);
+		System.out.println(str2);
+
+		System.out.println(str1=str2);
+	}
+}
+	
