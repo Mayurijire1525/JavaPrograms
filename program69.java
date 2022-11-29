a20c1b012fb100bb13123ac0d037e2f9b7d195cc
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-29 10:44:48 +0530

    String equal demo

---
+++ program69.java
@@ -0,0 +1,9 @@
+class stringde{
+	public static void main(String[]args){
+		String str1="c2w";
+		String str2="c2w";
+
+		System.out.println(str1=str2);
+		System.out.println(str1.equals(str2));
+	}
+}
