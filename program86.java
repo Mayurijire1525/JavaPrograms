76b86b684e84d1af195b03f976a3b89d1fd9d73e
Mayurijire1525 <Mayurijire441@gmail.com>
2022-12-03 14:25:49 +0530

    Instance blockprogram86.java

---
+++ program86.java
@@ -0,0 +1,10 @@
+class Instance{
+	{
+	System.out.println("In instance block");
+	}
+	public static void main(String[]args){
+		Instance obj=new Instance();
+		System.out.println("In main method");
+	}
+}
+
