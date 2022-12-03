9b589fdc735d9cd13c4724237dcaecbb3a6e0c30
Mayurijire1525 <Mayurijire441@gmail.com>
2022-12-03 13:50:59 +0530

    Stringdemoprogram81.java

---
+++ program81.java
@@ -0,0 +1,15 @@
+class Stringdemo{
+	public static void main(String[]args){
+		String str1=new String("Shashi");
+		String str2=new String("Bagal");
+
+		System.out.println(str1);
+		System.out.println(str2);
+
+		str1=str1.concat(str2);
+
+		System.out.println(str1);
+		System.out.println(str2);
+
+	}
+}
