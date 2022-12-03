86238412195c7688e5ea075c6cdcfe20d0a83d17
Mayurijire1525 <Mayurijire441@gmail.com>
2022-12-03 14:15:23 +0530

    static blockprogram84.java

---
+++ program84.java
@@ -0,0 +1,11 @@
+class Static{
+	static{
+		System.out.println("In static-1");
+	}
+	public static void main(String[]args){
+		System.out.println("In main");
+	}
+	static{
+		System.out.println("In static-2");
+	}
+}
