bd06c26acb4fa595f952a29334ca07d9dee67cb5
Mayurijire1525 <Mayurijire441@gmail.com>
2022-12-06 00:40:55 +0530

    polyprogram120.java

---
+++ program120.java
@@ -0,0 +1,15 @@
+class Demo{
+	void m1(int x,int y){
+		System.out.println("Int int");
+	}
+	void m1(float x,float y){
+		System.out.println("float float");
+	}
+}
+class Main{
+	public static void main(String[]args){
+		Demo d=new Demo();
+		d.m1(10,10);
+		d.m1(10.5f,10.5f);
+	}
+}
